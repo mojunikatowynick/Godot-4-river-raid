65b51c9e297bc529d6a5c0115ac45910d99cb512
DB <darek9410@op.pl>
2023-10-21 16:53:19 +0200

    bug fix, same property name

--- Control.gd
+++ Control.gd
@@ -9,16 +9,15 @@ func _ready():
 func display_key():
 	text = "%s" % InputMap.action_get_events(action)[0].as_text()
 
-func _on_toggled(button_pressed):
-	set_process_unhandled_key_input(button_pressed)
-	if button_pressed:
+func _on_toggled(myButtonPressed):
+	set_process_unhandled_key_input(myButtonPressed)
+	if myButtonPressed:
 		text = "..."
 	else:
 		display_key()
 		
 func _unhandled_key_input(event):
 	remap_key(event)
-	button_pressed = false
 	
 func remap_key(event):
 	InputMap.action_erase_events(action)
